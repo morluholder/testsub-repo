08f6eec9e000a5e1c50c13b5bcfa9e3c9af671e1
Morlu Holder <mholder.tiyoil@gmail.com>
2023-02-25 09:09:13 -0500

    my app sourcecode

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("b1").onclick = function () {
+    document.getElementById("paratext").style.display = "none"
+
+}
+
+document.getElementById("b2").onclick = function () {
+    document.getElementById("paratext").style.display = "inherit"
+
+}
